0db5e9966a8ba85561d10fa9851409f345797166
alorphan <a.lorphanapibul@wsu.edu>
2019-12-16 17:33:37 -0800

    Menu btn works

--- FrogKnight/Assets/Scripts/MainMenuController.cs
+++ FrogKnight/Assets/Scripts/MainMenuController.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class MainMenuController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+  /*  void Start()
     {
         
     }
@@ -15,4 +16,11 @@ public class MainMenuController : MonoBehaviour
     {
         
     }
+*/ 
+
+    public void goToScene()
+    { 
+        SceneManager.LoadScene("hub"); 
+    }
+        
 }
