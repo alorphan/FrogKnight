6e5422ebe44c7656faecfbaa94739ae416ad0ae4
alorphan <a.lorphanapibul@wsu.edu>
2019-12-22 11:24:29 -0800

    closing btn

---
+++ FrogKnight/Assets/Scripts/HubController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HubController : MonoBehaviour
+{
+    public GameObject expeditionPanel;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void openExpeditionPanel(GameObject expeditionPanel)
+    {
+        Debug.Log("hello");
+        expeditionPanel.gameObject.SetActive(true);
+    }
+
+    public void closeExpeditionPanel(GameObject expeditionPanel)
+    {
+        expeditionPanel.gameObject.SetActive(false);
+    }
+}
